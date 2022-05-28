2c5d6ace5ea17d430b6863a9ba5664c3f0b64dc5
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2022-05-28 07:05:49 -0500

    agregando AOS - scroll animation

--- src/components/Abilities/Abilities.js
+++ src/components/Abilities/Abilities.js
@@ -9,16 +9,19 @@ export default function Abilities() {
           Conoce nuestra forma de trabajo.
         </h2>
       </article>
-      <article className="mx-auto w-9/12 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="content-ico">
+      <article
+        data-aos="fade-right"
+        className="mx-auto w-9/12 grid sm:grid-cols-2 lg:grid-cols-3 gap-4"
+      >
+        <div data-aos="zoom-in-up" className="content-ico">
           <i class="mx-auto items-center justify-center rounded-md w-20 md:w-28 h-20 md:h-28 bg-blue-500 text-4xl md:text-6xl text-white fas fa-pencil-ruler"></i>
           <h1 className="text-xl md:text-3xl my-4">Diseño a la medida</h1>
         </div>
-        <div className="content-ico">
+        <div data-aos="zoom-in-up" className="content-ico">
           <i class="mx-auto items-center justify-center rounded-md w-20 md:w-28 h-20 md:h-28 bg-green-500 text-4xl md:text-6xl text-white fas fa-desktop"></i>
           <h1 className="text-xl md:text-3xl my-4">Diseño web</h1>
         </div>
-        <div className="content-ico">
+        <div data-aos="zoom-in-up" className="content-ico">
           <i class="mx-auto items-center justify-center rounded-md w-20 md:w-28 h-20 md:h-28 bg-yellow-400 text-4xl md:text-6xl text-white fas fa-icons"></i>
           <h1 className="text-xl md:text-3xl my-4">Contenido Multimedia </h1>
         </div>
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -18,15 +18,24 @@ export default function Slider() {
                       src={el.image}
                     />
                     <figcaption className="overflow-hidden px-14 md:px-24 justify-center text-white flex items-start flex-col h-96 md:h-96 bg-black-slider absolute z-10 top-0 right-0 text-left w-full md:w-1/2 py-8 md:py-0">
-                      <p className="font-bold">Proyectos</p>
-                      <h1 className="font-bold text-3xl lg:text-5xl typography">
+                      <p data-aos="zoom-in-up" className="font-bold">
+                        Proyectos
+                      </p>
+                      <h1
+                        data-aos="zoom-in-up"
+                        className="font-bold text-3xl lg:text-5xl typography"
+                      >
                         {el.title}
                       </h1>
-                      <p className="my-3 lg:my-5 leading-7 text-md lg:text-xl">
+                      <p
+                        data-aos="zoom-in-up"
+                        className="my-3 lg:my-5 leading-7 text-md lg:text-xl"
+                      >
                         {el.description}
                       </p>
                       <div className="flex items-center">
                         <a
+                          data-aos="zoom-in-up"
                           target="_blank"
                           className="hover:bg-black duration-300 hover:border-black hover:text-white text-black mr-2 h-9 rounded-md px-2 flex items-center justify-center border-2 border-white bg-white"
                           href={el.url}
